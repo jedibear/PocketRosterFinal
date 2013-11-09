b60804166026f0ce794d65e4daadc2059c921a95
jedibear <ryankulesza@dhcp-215-213.wireless.bowdoin.edu>
2013-11-09 17:15:29 -0500

    added images forbackgrounds

---
+++ Pocket Roster/GetCoaches.h
@@ -0,0 +1,18 @@
+//
+//  Pocket RosterViewController.h
+//  Pocket Roster
+//
+//  Created by Ryan Kulesza on 11/8/13.
+//  Copyright (c) 2013 Pocket Roster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetCoaches : NSObject
+
+
+
+//+(NSMutableDictionary *) getInfo: (NSString *) linkForURLSearch;
+//+(void) getInfo;
+
+@end
---
+++ Pocket Roster/GetCoaches.m
@@ -0,0 +1,42 @@
+//
+//  Pocket RosterViewController.m
+//  Pocket Roster
+//
+//  Created by Ryan Kulesza on 11/8/13.
+//  Copyright (c) 2013 Pocket Roster. All rights reserved.
+//
+
+#import "GetCoaches.h"
+
+@interface GetCoaches ()
+
+@end
+
+@implementation GetCoaches
+
++ (NSMutableDictionary *) getCoaches: (NSString *) linkForURLSearch
+//+(void) getInfo
+{
+    int key = 0;
+    NSString *urlStr = @"http://athletics.bowdoin.edu/sports/mxc/2013-14/roster";
+    NSURL *theURL = [[NSURL alloc] initWithString:urlStr];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:theURL];
+    
+    NSData *dataFromURL = [NSURLConnection sendSynchronousRequest:request returningResponse:NULL error:NULL];
+    
+    //get the data from the web page
+    NSString * htmlFromURL = [[NSString alloc] initWithData:dataFromURL encoding:NSASCIIStringEncoding];
+    
+    NSLog(@" %@", htmlFromURL);
+    
+    
+    NSString *stringBreakdown;
+    NSString *entireRoster;
+    
+    NSString *rosterElement1Final;
+    NSString *rosterElement1;
+}
+
+@end
