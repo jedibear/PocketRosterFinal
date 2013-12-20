36e4599bdeba8de851eaa0bb9d3ccabf7212c1ef
garvman <bowdoinfb1@gmail.com>
2013-12-20 00:26:12 -0500

    done mother fuckers~git add *git add *

--- Pocket Roster/HomeVC.h
+++ Pocket Roster/HomeVC.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
 
-@interface HomeVC : UIViewController
+@interface HomeVC : UIViewController <MFMailComposeViewControllerDelegate>
+
+- (IBAction)openMail:(id)sender;
 
 @end
--- Pocket Roster/HomeVC.m
+++ Pocket Roster/HomeVC.m
@@ -35,4 +35,54 @@
     return UIStatusBarStyleLightContent;
 }
 
+- (IBAction)openMail:(id)sender {
+    
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
+        mailer.mailComposeDelegate = self;
+        [mailer setSubject:@"A Message to BPR"];
+        NSArray *toRecipients = [NSArray arrayWithObjects:@"bowdoinpocketroster@gmail.com", nil];
+                                 [mailer setToRecipients:toRecipients];
+                                 
+                                 NSString *emailBody = @" ";
+                                 [mailer setMessageBody:emailBody isHTML:NO];
+                                 [self presentModalViewController:mailer animated:YES];
+                                 
+                                 }
+                                 else
+                                 {
+                                     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failure"
+                                                                                     message:@"Your device doesn't support the composer sheet"
+                                                                                    delegate:nil
+                                                                           cancelButtonTitle:@"OK"
+                                                                           otherButtonTitles: nil];
+                                     [alert show];
+                                     
+                                 }
+}
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled: you cancelled the operation and no email message was queued.");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved: you saved the email message in the drafts folder.");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail send: the email message is queued in the outbox. It is ready to send.");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail failed: the email message was not saved or queued, possibly due to an error.");
+            break;
+        default:
+            NSLog(@"Mail not sent.");
+            break;
+    }
+    // Remove the mail view
+    [self dismissModalViewControllerAnimated:YES];
+}
+
 @end
